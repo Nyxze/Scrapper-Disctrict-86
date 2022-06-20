66eaa3d142c7c88b3f84158501238100f8b5d401
Nyxze <manach.alexy@gmail.com>
2022-06-20 23:10:00 +0200

    Phase & Poule

--- scraper.js
+++ scraper.js
@@ -1,10 +1,9 @@
 const { JSDOM } = require("jsdom");
 const axios = require('axios');
-const base = 'https://foot86.fff.fr/competitions/';
+const BASE_URL = 'https://foot86.fff.fr/competitions/';
 
 const fs = require('fs');
 var dataArray = [];
-var championatArray = ["382649", "382650"];
 var objKeys = ["ranking", "teamName", "points", "days", "win", "nul", "loose", "forfait", "but_pour", "but_contre", "penality", "diff"];
 
 
@@ -24,20 +23,23 @@ const getChampionant = async () => {
 
     try {
         const { data } = await axios.get("https://foot86.fff.fr/competitions/");
-        const dom = new JSDOM(data, {
-            runScripts: "dangerously",
-            resources: "usable"
-        })
+        const dom = new JSDOM(data)
         const id = dom.window.document.querySelector("#select-ch");
         const champOptions = [...id.querySelectorAll('option')];
+
+
         champOptions.splice(0, 1);
-        champOptions.forEach((champSelect) => {
+
+        for (const option of champOptions) {
             let championat = {
-                name: champSelect.innerHTML,
-                id: champSelect.value
+                name: option.innerHTML,
+                id: option.value,
+                phase: await getPhase(option.value)
             }
-            championatArray.push(championat);
-        })
+            dataArray.push(championat);
+            console.log(dataArray);
+            await new Promise(r => setTimeout(r, 2000));
+        }
 
 
 
@@ -49,52 +51,103 @@ const getChampionant = async () => {
 
 }
 
+const getPhase = async (idOption) => {
 
 
-const yolo = async (champ) => {
 
     try {
+        console.log("in getPhase")
+        var phaseArray = [];
+        const { data } = await axios.get(`${BASE_URL}/?id=${idOption}&type=ch&tab=ranking`);
+        const dom = new JSDOM(data);
+
+        const id = dom.window.document.querySelector('#phase-competition');
+        const phaseOption = [...id.querySelectorAll('option')];
+
+
+        for (const option of phaseOption) {
+            let phase = {
+                "name": option.innerHTML,
+                "url": option.value.replace("resultat", "ranking"),
+                "poule": await getPoule(option.value.replace("resultat", "ranking"))
+
+            }
+            console.log(phase);
+            phaseArray.push(phase);
+        }
+        return phaseArray;
+    }
+    catch (error) {
+  
+    }
+
+}
+
+const getPoule = async (url) => {
+
+    console.log("inPOule")
+    try {
+
+        const { data } = await axios.get(`${BASE_URL}${url}`);
+        const dom = new JSDOM(data);
+        const id = dom.window.document.querySelector('#poule-competition');
+        const pouleOption = [...id.querySelectorAll('option')];
+
+        let pouleArray = [];
+        for (const option of pouleOption) {
+            let poule = {
+                "name": option.innerHTML,
+                "url": option.value.replace("resultat", "ranking"),
+                "team": await getTeams(option.value.replace("resultat", "ranking"))
+            }
+            pouleArray.push(poule);
+        }
+
+
+        return pouleArray;
+
+    } catch (err) {
+    
+    }
+
+}
+
+
+
+const getTeams = async (url) => {
+
+    try {
+        console.log("in getTeam")
 
         let clubs = [];
-        const { data } = await axios.get(`https://foot86.fff.fr/competitions/?id=${champ}&type=ch&tab=ranking`);
+        const { data } = await axios.get(`${BASE_URL}${url}`);
 
-        const dom = new JSDOM(data, {
-            runScripts: "dangerously",
-            resources: "usable"
-        });
-
+        const dom = new JSDOM(data);
         const clubCells = [...dom.window.document.querySelectorAll('table.ranking-tab tr')];
         clubCells.splice(0, 1);
-
-            clubCells.forEach( (data) => {
-          
+        for (const cell of clubCells) {
             let clubObject = {};
-            let td = data.querySelectorAll('td');
-            let test = [];
-            td.forEach((tdd) => {
-                test.push(tdd.textContent.trim());
-            })
-            for (let i = 0; i < data.childElementCount; i++) {
-                clubObject[objKeys[i]] = test[i];
+            let td = cell.querySelectorAll('td');
+            let textContentTd = [];
+            for (const cell of td) {
+                textContentTd.push(cell.textContent.trim())
             }
-             clubs.push(clubObject);
-    
-            
-        })
-    
-        let championat = {
-            name: champ.name,
-            id: champ.id,
-            teams: clubs
+
+            for (let i = 0; i < cell.childElementCount; i++) {
+                clubObject[objKeys[i]] = textContentTd[i];
+
+            }
+            clubs.push(clubObject);
+
         }
-        dataArray.push(championat);
-        //  writeData();
+
+        return clubs
 
     } catch (error) {
+    
         throw error;
     }
 
-    return;
 }
 
 
@@ -104,15 +157,8 @@ const uwu = async () => {
 
     try {
         await getChampionant();
-        championatArray.forEach(async (champ) => {
-            await yolo(champ);
-        })
-        // for(let i; i<championatArray.length; i++){
-        //     console.log(championatArray);
-        //     await yolo(championatArray[i.id]);
-        // }
+        writeData();
 
-        return;
     } catch (error) {
         throw error;
     }
